3df8cd704f02f687599022369aecd0e557950089
DhanviAthmakuri <athmakuri.1@osu.edu>
2017-11-20 23:58:11 -0500

    final commit by Dhanvi

--- nearFeaturePoints.m
+++ nearFeaturePoints.m
@@ -5,12 +5,14 @@ function [binary_count] = nearFeaturePoints(refx,refy,block,windowsize)
 binary_count = 0;
 count = 0;
 halfWindowSize = floor(windowsize/2);
+
 for i = 1:size(block,1)
     
     r = block(i,2);
     c = block(i,1);
     
-    if((r>=refx-halfWindowSize && r<=refx+halfWindowSize)&&(c>=refy-halfWindowSize && c<=refy+halfWindowSize))
+    if((r>=refx-halfWindowSize && r<=refx+halfWindowSize)&&...
+            (c>=refy-halfWindowSize && c<=refy+halfWindowSize))
         count  = count + 1;
     end
     
